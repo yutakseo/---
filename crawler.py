ba8128c16f0ceb517d69d703344fc006523cb35b
yutak <soano2428@gmail.com>
2023-09-28 18:38:41 +0900

    23.09.28

--- crawler.py
+++ crawler.py
@@ -11,11 +11,9 @@ if response.status_code == 200:
     soup = bs4.BeautifulSoup(html, 'html.parser')
     print("접속성공")
     
-    # 유효한 CSS 선택자를 사용하여 원하는 요소를 찾습니다.
     for i in range(10):
         temp = soup.select("div.table_data table tbody tr:nth-child(%d) td:nth-child(2)" %i)
         if temp:
-            # 내용이 비어 있지 않은지 확인합니다.
             contents.append(temp[0].get_text())
 else : 
     print(response.status_code)
