b475042b7d54b9173aac322c52817fd1b121de44
yutak <soano2428@gmail.com>
2023-09-30 16:18:54 +0900

    23.09.30

--- crawler.py
+++ crawler.py
@@ -2,24 +2,27 @@ import bs4
 import requests
 import re
 
-url = 'https://exam.toeic.co.kr/receipt/examSchList.php'
-contents = []
 
-response = requests.get(url)
-if response.status_code == 200:
-    html = response.text
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    print("접속성공")
-    
-    for i in range(10):
-        temp = soup.select("div.table_data table tbody tr:nth-child(%d) td:nth-child(2)" %i)
-        if temp:
-            contents.append(temp[0].get_text())
-else : 
-    print(response.status_code)
-    
+def crawler_machine(numb):
+    url = 'https://exam.toeic.co.kr/receipt/examSchList.php'
+    contents = []
 
-for i in range(len(contents)):
-    contents[i] = re.sub("\n", "", contents[i]).strip().replace("    ", "")
+    response = requests.get(url)
+    if response.status_code == 200:
+        html = response.text
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        print("접속성공")
+        
+        for i in range(10):
+            temp = soup.select("div.table_data table tbody tr:nth-child(%d) td:nth-child(2)" %i)
+            if temp:
+                contents.append(temp[0].get_text())
+    else : 
+        print(response.status_code)
+    for i in range(len(contents)):
+        contents[i] = re.sub("\n", "", contents[i]).strip().replace("    ", "")
     
-print(contents)
+    return contents[numb]
+
+
+print(crawler_machine(0))
